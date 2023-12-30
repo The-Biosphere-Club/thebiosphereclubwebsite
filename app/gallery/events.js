11bfe06ceaeb13e20f6329ba69fe560b02f5a644
Ojas Aklecha <ojasaklechayt@gmail.com>
2023-12-30 20:45:29 +0530

    Refactor image imports and update event titles in gallery page

---
+++ app/gallery/events.js
@@ -0,0 +1,239 @@
+// Import your image files here
+import bio1 from '../../assets/The Biosphere Mela/1.JPG';
+import bio2 from '../../assets/The Biosphere Mela/2.JPG';
+import bio3 from '../../assets/The Biosphere Mela/3.JPG';
+import bio4 from '../../assets/The Biosphere Mela/4.JPG';
+import bio5 from '../../assets/The Biosphere Mela/5.JPG';
+import bio6 from '../../assets/The Biosphere Mela/6.JPG';
+import bio7 from '../../assets/The Biosphere Mela/7.JPG';
+import bio8 from '../../assets/The Biosphere Mela/8.JPG';
+import bio9 from '../../assets/The Biosphere Mela/9.JPG';
+import bio10 from '../../assets/The Biosphere Mela/10.JPG';
+import dis1 from '../../assets/Descaptatio Natura/1.png';
+import dis2 from '../../assets/Descaptatio Natura/2.png';
+import dis3 from '../../assets/Descaptatio Natura/3.png';
+import dis4 from '../../assets/Descaptatio Natura/4.png';
+import dis5 from '../../assets/Descaptatio Natura/5.jpeg';
+import dis6 from '../../assets/Descaptatio Natura/6.jpeg';
+import dis7 from '../../assets/Descaptatio Natura/7.jpeg';
+import dis8 from '../../assets/Descaptatio Natura/8.jpeg';
+import expo1 from '../../assets/Expo/1.JPG';
+import expo2 from '../../assets/Expo/2.JPG';
+import expo3 from '../../assets/Expo/3.JPG';
+import expo4 from '../../assets/Expo/4.JPG';
+import expo5 from '../../assets/Expo/5.JPG';
+import expo6 from '../../assets/Expo/6.JPG';
+import expo7 from '../../assets/Expo/7.JPG';
+import expo8 from '../../assets/Expo/8.JPG';
+import expo9 from '../../assets/Expo/9.JPG';
+import expo10 from '../../assets/Expo/10.JPG';
+import expo11 from '../../assets/Expo/11.JPG';
+import expo12 from '../../assets/Expo/12.JPG';
+import expo13 from '../../assets/Expo/13.JPG';
+import green1 from '../../assets/Green Hands/1.jpg';
+import green2 from '../../assets/Green Hands/2.jpg';
+import green3 from '../../assets/Green Hands/3.jpg';
+import green4 from '../../assets/Green Hands/4.jpg';
+import green5 from '../../assets/Green Hands/5.jpg';
+import green6 from '../../assets/Green Hands/6.jpg';
+import green7 from '../../assets/Green Hands/7.jpg';
+import green8 from '../../assets/Green Hands/8.jpg';
+import green9 from '../../assets/Green Hands/9.jpg';
+import green10 from '../../assets/Green Hands/10.jpg';
+import green11 from '../../assets/Green Hands/11.jpg';
+import mic1 from '../../assets/Open Mic/1.jpg';
+import mic2 from '../../assets/Open Mic/2.JPG';
+import mic3 from '../../assets/Open Mic/3.JPG';
+import mic4 from '../../assets/Open Mic/4.JPG';
+import mic5 from '../../assets/Open Mic/5.jpg';
+import mic6 from '../../assets/Open Mic/6.jpg';
+import mic7 from '../../assets/Open Mic/7.JPG';
+import plant1 from '../../assets/Plantation Drive 2023/1.JPG';
+import plant2 from '../../assets/Plantation Drive 2023/2.JPG';
+import plant3 from '../../assets/Plantation Drive 2023/3.JPG';
+import plant4 from '../../assets/Plantation Drive 2023/4.JPG';
+import plant5 from '../../assets/Plantation Drive 2023/5.JPG';
+import plant6 from '../../assets/Plantation Drive 2023/6.JPG';
+import plant7 from '../../assets/Plantation Drive 2023/7.JPG';
+import plant8 from '../../assets/Plantation Drive 2023/8.JPG';
+import plant9 from '../../assets/Plantation Drive 2023/9.JPG';
+import plant10 from '../../assets/Plantation Drive 2023/10.JPG';
+import question1 from '../../assets/question with vejayanatham sir/1.png';
+import question2 from '../../assets/question with vejayanatham sir/2.png';
+import question3 from '../../assets/question with vejayanatham sir/3.jpg';
+import frame1 from '../../assets/Wildlife Frames/1.JPG';
+import frame2 from '../../assets/Wildlife Frames/2.JPG';
+import frame3 from '../../assets/Wildlife Frames/3.JPG';
+import frame4 from '../../assets/Wildlife Frames/4.JPG';
+import frame5 from '../../assets/Wildlife Frames/5.JPG';
+import frame6 from '../../assets/Wildlife Frames/6.JPG';
+import frame7 from '../../assets/Wildlife Frames/7.JPG';
+import sparrow1 from '../../assets/Sparrow Session Best Photos/1.JPG';
+import sparrow2 from '../../assets/Sparrow Session Best Photos/2.JPG';
+import sparrow3 from '../../assets/Sparrow Session Best Photos/3.JPG';
+import sparrow4 from '../../assets/Sparrow Session Best Photos/4.JPG';
+import sparrow5 from '../../assets/Sparrow Session Best Photos/5.JPG';
+import sparrow6 from '../../assets/Sparrow Session Best Photos/6.JPG';
+import sparrow7 from '../../assets/Sparrow Session Best Photos/7.JPG';
+import sparrow8 from '../../assets/Sparrow Session Best Photos/8.JPG';
+import sparrow9 from '../../assets/Sparrow Session Best Photos/9.JPG';
+import sparrow10 from '../../assets/Sparrow Session Best Photos/10.JPG';
+import sparrow11 from '../../assets/Sparrow Session Best Photos/11.JPG';
+import agri1 from '../../assets/Saga of indian agriculture/1.jpeg';
+import agri2 from '../../assets/Saga of indian agriculture/2.png';
+import agri3 from '../../assets/Saga of indian agriculture/3.png';
+import agri4 from '../../assets/Saga of indian agriculture/4.png';
+import agri5 from '../../assets/Saga of indian agriculture/5.png';
+import agri6 from '../../assets/Saga of indian agriculture/6.png';
+import agri7 from '../../assets/Saga of indian agriculture/7.jpeg';
+import agri8 from '../../assets/Saga of indian agriculture/8.jpeg';
+import agri9 from '../../assets/Saga of indian agriculture/9.jpeg';
+import agri10 from '../../assets/Saga of indian agriculture/10.jpeg';
+import agri11 from '../../assets/Saga of indian agriculture/11.jpeg';
+
+// Define the images array
+const images = [
+    {
+        title: 'The Biosphere Mela',
+        images: [
+            bio1,
+            bio2,
+            bio3,
+            bio4,
+            bio5,
+            bio6,
+            bio7,
+            bio8,
+            bio9,
+            bio10,
+        ]
+    },
+    {
+        title: 'Descaptatio Natura',
+        images: [
+            dis1,
+            dis2,
+            dis3,
+            dis4,
+            dis5,
+            dis6,
+            dis7,
+            dis8,
+        ]
+    },
+    {
+        title: 'Expo',
+        images: [
+            expo1,
+            expo2,
+            expo3,
+            expo4,
+            expo5,
+            expo6,
+            expo7,
+            expo8,
+            expo9,
+            expo10,
+            expo11,
+            expo12,
+            expo13,
+        ]
+    },
+    {
+        title: 'Green Hands',
+        images: [
+            green1,
+            green2,
+            green3,
+            green4,
+            green5,
+            green6,
+            green7,
+            green8,
+            green9,
+            green10,
+            green11,
+        ]
+    },
+    {
+        title: 'Open Mic',
+        images: [
+            mic1,
+            mic2,
+            mic3,
+            mic4,
+            mic5,
+            mic6,
+            mic7,
+        ]
+    },
+    {
+        title: 'Plantation Drive 2023',
+        images: [
+            plant1,
+            plant2,
+            plant3,
+            plant4,
+            plant5,
+            plant6,
+            plant7,
+            plant8,
+            plant9,
+            plant10,
+        ]
+    },
+    {
+        title: 'Questionnaire with Venjayanatham Sir',
+        images: [
+            question1,
+            question2,
+            question3,
+        ]
+    },
+    {
+        title: 'Saga of Indian Agriculture',
+        images: [
+            agri1,
+            agri2,
+            agri3,
+            agri4,
+            agri5,
+            agri6,
+            agri7,
+            agri8,
+            agri9,
+            agri10,
+            agri11,
+        ]
+    },
+    {
+        title: 'Sparrow Session',
+        images: [
+            sparrow1,
+            sparrow2,
+            sparrow3,
+            sparrow4,
+            sparrow5,
+            sparrow6,
+            sparrow7,
+            sparrow8,
+            sparrow9,
+            sparrow10,
+            sparrow11,
+        ]
+    },
+    {
+        title: 'Wildlife Frames',
+        images: [
+            frame1,
+            frame2,
+            frame3,
+            frame4,
+            frame5,
+            frame6,
+            frame7,
+        ]
+    },
+];
+
+export default images;
